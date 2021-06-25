bbc72cd89ea22ea1e622db23e7b9eb7d848393aa
Nidhisha Shetty <shetty3491@gmail.com>
2021-06-25 23:10:49 +0530

    Add files via upload

---
+++ LinkedList/linkedlist_traversal.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, data):
+        self.data = data #Assign data
+        self.next=None
+        
+class LinkedList:
+    def __init__(self):
+        self.a = None  
+
+    def printlinkedlist(self):
+        node=self.a
+        while node!=None:
+            print(node.data)
+            node=node.next
+
+ll=LinkedList()
+
+ll.a=Node(1)
+ll.b=Node(2)
+ll.c=Node(3)
+ll.d=Node(4)
+ll.e=Node(5)
+
+ll.a.next=ll.b
+ll.b.next=ll.c
+ll.c.next=ll.d 
+ll.d.next=ll.e 
+
+ll.printlinkedlist()
+
