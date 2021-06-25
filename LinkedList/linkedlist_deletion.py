2f7054d207b911fb7e55dac82f6314a39b976703
Nidhisha Shetty <shetty3491@gmail.com>
2021-06-25 22:15:15 +0530

    Update linkedlist_deletion.py

--- LinkedList/linkedlist_deletion.py
+++ LinkedList/linkedlist_deletion.py
@@ -13,18 +13,21 @@ class LinkedList:
             print(temp.data)
             temp=temp.next
 
+    #Delete the first node of the LnkedList        
     def removefirstnode(self):
         if self.a==None:
             print("Lonkedlist is empty")
         else:
             self.a=self.a.next
-
+            
+    #Delete the last node of the LnkedList  
     def removelastnode(self):
         node=self.a
         while node.next.next!=None:
             node=node.next
         node.next=None
 
+    #Delete a specific node from the LnkedList  
     def removebetnode(self, nodecheck):
         if self.a==None:
             print("Linkedlist is empty")
@@ -36,6 +39,7 @@ class LinkedList:
 
 ll=LinkedList()
 
+#Creating nodes in the linkedlist
 ll.a=Node(1)
 ll.b=Node(2)
 ll.c=Node(3)
@@ -43,24 +47,22 @@ ll.d=Node(4)
 ll.e=Node(5)
 ll.f=Node(6)
 
+#Connecting the nodes in the linkedlist
 ll.a.next=ll.b
 ll.b.next=ll.c
 ll.c.next=ll.d
 ll.d.next=ll.e
 ll.e.next=ll.f
 
-ll.printlist()
-print("--------")
+ll.printlist() #Prints the complete linkedlist
 
-ll.removefirstnode()
-ll.printlist()
+ll.removefirstnode() 
+ll.printlist() #Prints the linkedlist except first node
 print("--------")
 
 ll.removelastnode()
-ll.printlist()
+ll.printlist() #Prints the linkedlist except first and last node 
 print("--------")
 
 ll.removebetnode(4)
-ll.printlist()
-
-
+ll.printlist() #Prints the linkedlist except first, last, and the node with value 4
