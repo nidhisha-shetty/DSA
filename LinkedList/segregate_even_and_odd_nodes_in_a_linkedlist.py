ba331539a5d4cc52e5b7a024cd96505c8167f445
Nidhisha Shetty <shetty3491@gmail.com>
2021-07-12 23:08:43 +0530

    Add files via upload

---
+++ LinkedList/segregate_even_and_odd_nodes_in_a_linkedlist.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.a=None
+    
+    def traverse(self):
+        arr=[]
+        arr_odd=[]
+        arr_even=[]
+        temp=self.a
+        while temp!=None:
+            print(temp.data)
+            arr.append(temp.data)
+            temp=temp.next
+        
+        for node in arr:
+            if node%2==0:
+                arr_even.append(node)
+            else:
+                arr_odd.append(node)
+        arr_even.extend(arr_odd)
+        print(arr_even)
+
+ll=LinkedList()
+ll.a=Node(8)
+ll.b=Node(12)
+ll.c=Node(10)
+ll.d=Node(5)
+ll.e=Node(4)
+ll.f=Node(1)
+ll.g=Node(6)
+
+ll.a.next=ll.b
+ll.b.next=ll.c
+ll.c.next=ll.d
+ll.d.next=ll.e
+ll.e.next=ll.f
+ll.f.next=ll.g
+
+ll.traverse()
