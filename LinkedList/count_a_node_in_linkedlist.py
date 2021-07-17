049df338a3f378f23608c6556bb3d0aea1029749
Nidhisha Shetty <shetty3491@gmail.com>
2021-07-18 00:10:33 +0530

    Update count_a_node_in_linkedlist.py

--- LinkedList/count_a_node_in_linkedlist.py
+++ LinkedList/count_a_node_in_linkedlist.py
@@ -1,4 +1,6 @@
-#P.S: Write a function that counts the number of times a given int occurs in a Linked List
+#P.S: Write a function that counts the number of times a given int occurs in a Linked List using iterative and recursive methods
+
+#using iterative method
 class Node:
     def __init__(self, data):
         self.data=data
@@ -33,3 +35,43 @@ ll.e.next=ll.f
 ll.f.next=ll.g
 
 print(ll.count(1))
+
+#using recursive method
+class Node:
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+    
+class LinkedList:
+    def __init__(self):
+        self.a=None
+        self.check=0 
+    def count(self, head, node):
+        temp=head
+        
+        if temp==None:
+            return self.check
+        if temp.data==node:
+            self.check+=1
+            temp1=temp.next
+            return self.count(temp1, node)
+        else:
+            temp1=temp.next
+            return self.count(temp1, node)
+ll=LinkedList()
+ll.a=Node(1)
+ll.b=Node(2)
+ll.c=Node(1)
+ll.d=Node(2)
+ll.e=Node(1)
+ll.f=Node(3)
+ll.g=Node(1)
+
+ll.a.next=ll.b
+ll.b.next=ll.c 
+ll.c.next=ll.d 
+ll.d.next=ll.e
+ll.e.next=ll.f
+ll.f.next=ll.g
+
+print(ll.count(ll.a,1))
