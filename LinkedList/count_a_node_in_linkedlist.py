c6dbd69bee7d4dc35679a9907651dccbab0fbacf
Nidhisha Shetty <shetty3491@gmail.com>
2021-07-17 23:17:50 +0530

    Add files via upload

---
+++ LinkedList/count_a_node_in_linkedlist.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+    
+class LinkedList:
+    def __init__(self):
+        self.a=None
+    def count(self, node):
+        check=0
+        temp=self.a
+        while temp!=None:
+            if temp.data==node:
+                check+=1
+            temp=temp.next
+        return check
+
+ll=LinkedList()
+ll.a=Node(1)
+ll.b=Node(2)
+ll.c=Node(1)
+ll.d=Node(2)
+ll.e=Node(1)
+ll.f=Node(3)
+ll.g=Node(1)
+
+ll.a.next=ll.b
+ll.b.next=ll.c 
+ll.c.next=ll.d 
+ll.d.next=ll.e
+ll.e.next=ll.f
+ll.f.next=ll.g
+
+print(ll.count(1))
