567e6fa3ca01806f5f515df7317ddf580bc9cde0
Nidhisha Shetty <shetty3491@gmail.com>
2021-07-19 21:40:56 +0530

    Update get_nth_node_in_linkedlist.py

--- LinkedList/get_nth_node_in_linkedlist.py
+++ LinkedList/get_nth_node_in_linkedlist.py
@@ -1,3 +1,4 @@
+#iterative way
 class Node:
     def __init__(self, data):
         self.data=data
@@ -32,4 +33,42 @@ ll.c.next=ll.d
 ll.d.next=ll.e 
 
 print(ll.get_node_data(0))
+
+
+#recursive way
+class Node:
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.a=None
+        self.count=0 
+        
+    def get_node_data(self, index):
+        
+        while self.a!=None:
+            if self.count==index:
+                return self.a.data
+            else:
+                self.count+=1             
+                self.a=self.a.next
+                self.get_node_data(index)
+
+ll=LinkedList()
+
+ll.a=Node(1)
+ll.b=Node(2)
+ll.c=Node(3)
+ll.d=Node(4)
+ll.e=Node(5)
+
+ll.a.next=ll.b
+ll.b.next=ll.c 
+ll.c.next=ll.d 
+ll.d.next=ll.e 
+
+print(ll.get_node_data(3))
+
 
