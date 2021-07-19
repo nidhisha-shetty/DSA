142acd6b05d3fbdc683c2d7b80713ac04a9b54b9
Nidhisha Shetty <shetty3491@gmail.com>
2021-07-19 21:12:18 +0530

    Add files via upload

---
+++ LinkedList/get_nth_node_in_linkedlist.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.a=None 
+    def get_node_data(self, index):
+        temp=self.a 
+        count=0
+        while temp!=None:
+            
+            if count==index:
+                return temp.data 
+            else:
+                count+=1
+                temp=temp.next
+        return "Invalid index"
+
+ll=LinkedList()
+
+ll.a=Node(1)
+ll.b=Node(2)
+ll.c=Node(3)
+ll.d=Node(4)
+ll.e=Node(5)
+
+ll.a.next=ll.b
+ll.b.next=ll.c 
+ll.c.next=ll.d 
+ll.d.next=ll.e 
+
+print(ll.get_node_data(0))
+
