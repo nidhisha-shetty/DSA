eaf53c3c690c4e11db826af86c50f9860725bafd
Nidhisha Shetty <shetty3491@gmail.com>
2021-09-06 22:38:26 +0530

    Update circular_llinkedlist_all_operations.py

--- LinkedList/circular_linkedlist/circular_llinkedlist_all_operations.py
+++ LinkedList/circular_linkedlist/circular_llinkedlist_all_operations.py
@@ -15,8 +15,7 @@ class LinkedList:
             temp=temp.next
     
     def addToEmpty(self, data):
-
-		# Creating the newnode new
+	# Creating the newnode new
         new = Node(data)
         self.a = new
     
@@ -34,10 +33,27 @@ class LinkedList:
         self.a = temp
         # print(self.a.data)
         return self.a
-    
+
+    def addAfter(self, data, item):
+        temp = Node(data)
+        p = self.a.next
+        while p:
+            if (p.data == item):
+                temp.next = p.next
+                p.next = temp
+            if (p == self.a):
+                self.a = temp
+                return self.a
+            else:
+                return self.a
+            p = p.next
+            if (p == self.a.next):
+                print(item, "not present in the list")
+                break
             
 ll=LinkedList()
 ll.addToEmpty(3)
 ll.addBegin(5)
 ll.addEnd(7)
+ll.addAfter(9, 5)
 ll.traverse()
