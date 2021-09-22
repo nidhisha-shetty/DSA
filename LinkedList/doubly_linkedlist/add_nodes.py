ace0ab45e5e9efec279c9e976ce0ba30da09f092
Nidhisha Shetty <shetty3491@gmail.com>
2021-09-22 23:33:23 +0530

    Update add_nodes.py

--- LinkedList/doubly_linkedlist/add_nodes.py
+++ LinkedList/doubly_linkedlist/add_nodes.py
@@ -12,17 +12,22 @@ class Linkedlist:
         while temp!=None:
             print("node: "+str(temp.data))
             temp=temp.next
+    '''
+    Add start node
     #2->4
     #1->2->4
+    '''
     def add_start_node(self, data):
         new_node=Node(data)
         new_node.next=self.head
         new_node.prev=None
         self.head.prev=new_node
         self.head=new_node
-
+    '''
+    Add a node after a given node
     #1->2->4
     #1->2->3->4
+    '''
     def insertAfter(self, cur_data, next_data):
         temp=self.head 
         while temp.data!=cur_data:
@@ -33,9 +38,11 @@ class Linkedlist:
         after_node.next=temp.next 
         temp.next=after_node
         after_node.prev=temp
-
+    '''
+    Add a node before a given node
     #1->2->3->4
     #1->2->5->3->4
+    '''
     def insertBefore(self, cur_data, before_data):
         temp=self.head 
         while temp.next.data!=cur_data:
@@ -45,9 +52,11 @@ class Linkedlist:
         before_node.next=temp.next
         temp.next=before_node
         before_node.prev=temp  
-
+    '''
+    Add node at end
     #1->2->5->3->4
     #1->2->5->3->4->10
+    '''
     def end_node(self, end_data):
         temp=self.head 
         while temp.next!=None:
