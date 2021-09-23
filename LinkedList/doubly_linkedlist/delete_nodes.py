17ee3eaf1e70f06944e51ae438a1c64616bd6609
Nidhisha Shetty <shetty3491@gmail.com>
2021-09-23 23:41:04 +0530

    Create delete_nodes.py

---
+++ LinkedList/doubly_linkedlist/delete_nodes.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self,data):
+        self.next=None 
+        self.prev=None
+        self.data=data 
+
+class Linkedlist:
+    def __init__(self):
+        self.head=None 
+    def traverse(self):
+        temp=self.head
+        while temp!=None:
+            print("node: "+str(temp.data))
+            temp=temp.next
+    
+    def delete_start_node(self, del_data):
+        del_node=Node(del_data)
+        temp=self.head
+        while temp!=None:
+            if self.head.data==del_node.data:
+                self.head=temp.next
+                temp.next.prev=None
+                break
+        # return self.head
+
+ll=Linkedlist()
+ll.head=Node(1)
+ll.b=Node(2)
+ll.c=Node(3) 
+
+ll.head.next=ll.b 
+ll.b.next=ll.c 
+
+ll.delete_start_node(1)
+ll.traverse()
