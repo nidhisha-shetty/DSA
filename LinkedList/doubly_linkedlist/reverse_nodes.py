cc8e7837f4ee9d5db254e2bac2e4822a375c93c7
Nidhisha Shetty <shetty3491@gmail.com>
2021-09-30 21:09:46 +0530

    Update reverse_nodes.py

--- LinkedList/doubly_linkedlist/reverse_nodes.py
+++ LinkedList/doubly_linkedlist/reverse_nodes.py
@@ -34,3 +34,54 @@ ll.e.next=ll.f
 ll.f.next=ll.g 
 
 ll.traverse_reverse()
+
+#without using list
+class Node:
+    def __init__(self,data):
+        self.next=None 
+        self.prev=None
+        self.data=data 
+
+class Linkedlist:
+    def __init__(self):
+        self.head=None 
+
+    def traverse(self):
+        
+        temp=self.head 
+        while(temp!=None):
+            print(temp.data)
+            temp=temp.next
+    
+    def reverse(self):
+        temp = None
+        current = self.head
+		
+        while current is not None:
+            temp = current.prev
+            current.prev = current.next
+            current.next = temp
+            current = current.prev
+
+        if temp is not None:
+            self.head = temp.prev
+        
+        
+ll=Linkedlist()
+ll.head=Node(1)
+ll.b=Node(2)
+ll.c=Node(3) 
+ll.d=Node(4)
+ll.e=Node(5)
+ll.f=Node(6)
+ll.g=Node(7)
+
+ll.head.next=ll.b 
+ll.b.next=ll.c 
+ll.c.next=ll.d
+ll.d.next=ll.e
+ll.e.next=ll.f 
+ll.f.next=ll.g 
+
+ll.reverse()
+ll.traverse()
