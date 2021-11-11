8e6c8fc99ff8c852a0bc3292ca136b56f0d79490
Nidhisha Shetty <shetty3491@gmail.com>
2021-11-11 21:06:57 +0530

    linkedlist updation program

---
+++ LinkedList/linear_linkedlist/ll_traverse.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.a=None
+    def traverse(self):
+        head=self.a
+        while head!=None:
+            print(head.data)
+            head=head.next
+    def update(self, existing, new):
+        head=self.a
+        while head.data!=existing:
+            head=head.next
+        head.data=new
+
+ll=LinkedList()
+
+ll.a=Node(1)
+ll.b=Node(2)
+ll.c=Node(3)
+ll.d=Node(4)
+ll.e=Node(5)
+
+ll.a.next=ll.b
+ll.b.next=ll.c
+ll.c.next=ll.d 
+ll.d.next=ll.e
+
+# ll.traverse()
+ll.update(5,7)
+ll.traverse()
