39da3b9325ac86d556b9f1fad93f7621599b53f4
Nidhisha Shetty <shetty3491@gmail.com>
2021-12-28 22:43:59 +0530

    Create test_update_nodes.py

---
+++ LinkedList/doubly_linkedlist/test_update_nodes.py
@@ -0,0 +1,19 @@
+import pytest
+
+from doubly_linked_list_update import Linkedlist
+
+class TestLinkedlist:
+    
+    def setup_method(self):
+        """[12, 8, 2, 5]"""
+        self.prepared_linked_list = Linkedlist()
+        self.prepared_linked_list.add_start_node(5)
+        self.prepared_linked_list.add_start_node(2)
+        self.prepared_linked_list.add_start_node(8)
+        self.prepared_linked_list.add_start_node(12)
+        # self.prepared_linked_list.insertAfter(2,6)
+        assert list(self.prepared_linked_list) == [12, 8, 2, 5]
+    
+    def test_update_node(self):
+        self.prepared_linked_list.update_node(8, 7)
+        assert list(self.prepared_linked_list) == [12, 7, 2, 5]
