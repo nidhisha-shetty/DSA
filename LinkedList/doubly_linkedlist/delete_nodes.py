49ef23a743b09dad81f7053eb1e4e0196240365c
Nidhisha Shetty <shetty3491@gmail.com>
2021-12-30 22:23:47 +0530

    Update delete_nodes.py

--- LinkedList/doubly_linkedlist/delete_nodes.py
+++ LinkedList/doubly_linkedlist/delete_nodes.py
@@ -1,77 +1,3 @@
-# class Node:
-#     def __init__(self,data):
-#         self.next=None 
-#         self.prev=None
-#         self.data=data 
-
-# class Linkedlist:
-#     def __init__(self):
-#         self.head=None
-
-#     def traverse(self):
-#         temp=self.head 
-#         prev=self.head
-#         while temp!=None:
-#             # print("prev node"+str(prev.data))
-#             print("node: "+str(temp.data))
-#             prev=temp
-#             temp=temp.next
-            
-#     def delete_start_node(self):
-#         temp=self.head
-#         self.head=temp.next
-#         temp.next.prev=None
-    
-#     def delete_before_node(self,data):
-#         temp=self.head 
-#         prev=self.head
-#         while temp.next.data!=data:
-#             prev=temp
-#             temp=temp.next
-#         prev.next=temp.next
-#         temp.next.prev=prev 
-
-#     def delete_after_node(self,data):
-#         temp=self.head 
-#         # prev=self.head
-#         while temp.data!=data:
-#             # prev=temp
-#             temp=temp.next
-#         temp.next=temp.next.next
-        
-#     def delete_end_node(self):
-#         temp=self.head
-#         while temp.next.next!=None:
-#             # print(temp.next.data)
-#             temp=temp.next
-#         # print(temp.data)    
-#         temp.next=None
-#         # print(temp)
-
-# ll=Linkedlist()
-# ll.head=Node(1)
-# ll.b=Node(2)
-# ll.c=Node(3) 
-# ll.d=Node(4)
-# ll.e=Node(5)
-# ll.f=Node(6)
-# ll.g=Node(7)
-
-# ll.head.next=ll.b 
-# ll.b.next=ll.c 
-# ll.c.next=ll.d
-# ll.d.next=ll.e
-# ll.e.next=ll.f 
-# ll.f.next=ll.g 
-
-# ll.delete_start_node()
-# ll.delete_before_node(5)
-# ll.delete_after_node(2)
-# ll.delete_end_node()
-# ll.traverse()
-
-
-
 class Node:
     def __init__(self,data):
         self.next=None 
@@ -85,7 +11,6 @@ class Linkedlist:
     def add_start_node(self, data):
         new_node=Node(data)
         if self.head != None: 
-            
             new_node.next=self.head
             new_node.prev=None
             self.head.prev=new_node
@@ -95,7 +20,6 @@ class Linkedlist:
             self.head = new_node
             self.tail = new_node
             new_node.prev = None # There's only one element so both pointers refer to null
-
 
     def delete_node(self, old_data):
         node = self.head
